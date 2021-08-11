94de53df09b629d4a51dccef274ba4db46e19ffa
Trinadh <stkumar@qualminds.com>
2021-08-11 19:07:38 +0530

    eShop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,  VERSION, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eShop';
+  breakpoint: number;
+  ngVersion: string = VERSION.full;
+  constructor() { }
+  
+    ngOnInit() {
+      this.breakpoint = (window.innerWidth <= 400) ? 1 : 3;
+    }
+    onResize(event) {
+      this.breakpoint = (event.target.innerWidth <= 400) ? 1 : 6;
+    }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import { FormsModule } from '@angular/forms';
 
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    FormsModule,
+    MatToolbarModule
+    
+    
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
